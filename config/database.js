923427fcc0ea9d59a442b39774c0bd3f112fd285
David Thompson <drthusker@gmail.com>
2018-02-19 17:01:33 -0700

    JawsDB

--- config/database.js
+++ config/database.js
@@ -1,10 +1,26 @@
 // config/database.js
+
+ //    'connection': {
+ //        'host': 'localhost',
+ //        'user': 'root',
+ //        'password': 'root'
+ //    },
+	// 'database': 'my_schema',
+ //    'users_table': 'users'
 module.exports = {
-    'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'root'
-    },
-	'database': 'my_schema',
-    'users_table': 'users'
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
 };
